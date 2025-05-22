813468f40ce1aafc6724c60a25e78e20bb85cab0
Mikhail Silaev <mikesilaev@gmail.com>
2025-05-22 08:37:53 +0300

    homework2

---
+++ 02-experiment-tracking/homework/train.py
@@ -0,0 +1,44 @@
+import os
+import pickle
+import click
+import mlflow
+import numpy as np
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+
+    #mlflow.set_tracking_uri("sqlite:///mlflow2.db")
+    #mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("nyc-taxi-experiment_hw2_q3")
+
+    with mlflow.start_run(run_name="RandomForest") as run:
+
+        mlflow.sklearn.autolog()
+
+
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred))
+
+
+if __name__ == '__main__':
+    run_train()
